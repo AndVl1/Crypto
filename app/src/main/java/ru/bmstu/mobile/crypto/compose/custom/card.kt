4fa8fe5371f0ee51f6c6684dc75342cd163000c3
Andrey Vladislavov <a.vladislavov@corp.mail.ru>
2021-10-23 18:09:10 +0300

    theme

--- app/src/main/java/ru/bmstu/mobile/crypto/compose/custom/card.kt
+++ app/src/main/java/ru/bmstu/mobile/crypto/compose/custom/card.kt
@@ -1,44 +1,32 @@
 package ru.bmstu.mobile.crypto.compose.custom
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import ru.bmstu.mobile.crypto.compose.theme.CryptoTheme
 
 @Composable
 fun ClickableCard(
     modifier: Modifier,
     elevation: Dp = 2.dp,
-    shape: Shape = RoundedCornerShape(2.dp),
-    backgroundColor: Color = MaterialTheme.colors.surface,
-    contentColor: Color = contentColorFor(backgroundColor),
-    onClick: () -> Unit,
-    content: @Composable () -> Unit
+    shape: Shape = CryptoTheme.shape.cornerStyle,
+    backgroundColor: Color = CryptoTheme.colors.secondaryBackground,
+    contentColor: Color = CryptoTheme.colors.primaryText,
+    onClick: () -> Unit = {},
+    content: @Composable () -> Unit,
 ) {
-    Box(
-        modifier = Modifier
-            .focusable()
-            .clickable { onClick() },
-        contentAlignment = Alignment.Center
+    Card(
+        modifier = modifier.clickable { onClick() },
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        elevation = elevation,
+        shape = shape,
     ) {
-        Card(
-            modifier = modifier,
-            backgroundColor = backgroundColor,
-            contentColor = contentColor,
-            elevation = elevation,
-            shape = shape,
-        ) {
-            content.invoke()
-        }
+        content.invoke()
     }
 }
